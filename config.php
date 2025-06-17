46d548a95139665040b32920522e395a6de96613
Modi97 <odhisjoseph82@gmail.com>
2025-06-17 15:40:54 +0300

    Update config.php

--- config.php
+++ config.php
@@ -1,6 +1,6 @@
 <?php
 // Google OAuth configuration
 
-const GOOGLE_CLIENT_ID = 'YOUR_GOOGLE_CLIENT_ID';
-const GOOGLE_CLIENT_SECRET = 'YOUR_GOOGLE_CLIENT_SECRET';
-const GOOGLE_REDIRECT_URI = 'http://localhost/callback.php';
+const GOOGLE_CLIENT_ID = '578350091421-m70c40bijt06ev0haudi6967nhqpnhvp.apps.googleusercontent.com';
+const GOOGLE_CLIENT_SECRET = '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';
+const GOOGLE_REDIRECT_URI = 'https://3d66-41-89-65-175.ngrok-free.app/callback.php';
