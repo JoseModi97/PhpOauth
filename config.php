d2f33672a3dd5713b66b14b65ae294762eae329f
Modi97 <102935099+JoseModi97@users.noreply.github.com>
2025-06-17 15:20:50 +0300

    Add Google OAuth example

---
+++ config.php
@@ -0,0 +1,6 @@
+<?php
+// Google OAuth configuration
+
+const GOOGLE_CLIENT_ID = 'YOUR_GOOGLE_CLIENT_ID';
+const GOOGLE_CLIENT_SECRET = 'YOUR_GOOGLE_CLIENT_SECRET';
+const GOOGLE_REDIRECT_URI = 'http://localhost/callback.php';
